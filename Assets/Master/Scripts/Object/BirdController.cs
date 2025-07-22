ae9a64d1070620f168c6746ae4f18a47768d0b30
Thuan <thuanm.ot@gmail.com>
2025-07-22 15:26:27 +0700

    Update bird flying with mouse events

--- Assets/Master/Scripts/Object/BirdController.cs
+++ Assets/Master/Scripts/Object/BirdController.cs
@@ -15,7 +15,7 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) 
         {
             Flap();
         }
